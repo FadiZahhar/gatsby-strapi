3def61d20eeb72585f9000f519523e92c49f4056
Fadi Zahhar <79freelancer@gmail.com>
2022-01-11 18:03:48 +0200

    completing the project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ const Projects = ({ projects, title, showLink }) => {
     <section className="section projects">
       <Title title={title} />
       <div className="section-center projects-center">
-        {projects?.map((project, index) => {
+        {projects.map((project, index) => {
           return <Project key={project.id} index={index} {...project} />
         })}
       </div>
