42e75958bc83cf052593b77b82e25e043e8b3f1d
Fadi Zahhar <79freelancer@gmail.com>
2022-01-11 14:47:41 +0200

    fixing the project index to show properly

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ const Projects = ({projects,title,showLink}) => {
   return <section className="section projects">
     <Title title={title} />
     <div className="section-center projects-center">
-      {projects.map((project) => {
+      {projects.map((project,index) => {
         return <Project key={project.id} index={index} {...project} />
       })}
     </div>
